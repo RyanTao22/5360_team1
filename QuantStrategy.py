82802396675bd50bb1f451ec4ccebc7a9d51d502
Wesley Lau <wesleyklhk@yahoo.com.hk>
2024-07-27 23:48:05 +0800

    merged quant strategy

--- QuantStrategy.py
+++ QuantStrategy.py
@@ -12,18 +12,13 @@ from typing import Mapping
 from uuid import uuid1
 
 import pandas as pd
-import numpy as np
+
 from common.OrderBookSnapshot_FiveLevels import OrderBookSnapshot_FiveLevels
 from common.Platform.OrderManager import OrderManager
 from common.Strategy import Strategy
 from common.SingleStockOrder import SingleStockOrder
 from common.SingleStockExecution import SingleStockExecution
 from datetime import datetime
-import lightgbm as lgb
-from collections import deque
-from copulae.elliptical import GaussianCopula
-from scipy.stats import rankdata, norm
-
 class QuantStrategy(Strategy):
     
     def __init__(self, stratID, stratName, stratAuthor, ticker, day):
@@ -47,7 +42,7 @@ class QuantStrategy(Strategy):
             return SingleStockOrder('testTicker','2019-07-05',time.asctime(time.localtime(time.time())))
         else:
             return None
-        
+                
 
 class SampleDummyStrategy(QuantStrategy):
     def __init__(self, stratID, stratName, stratAuthor, day, ticker,
@@ -72,16 +67,18 @@ class SampleDummyStrategy(QuantStrategy):
         self.timestamp = []
 
     def run(self,execution:SingleStockExecution)->list[SingleStockOrder]:
-        ticker1 = self.ticker[0]
-        ticker2 = self.ticker[1]
+        ticker1 = "2610"
+        ticker2 = "NEF1"        
         if execution is None:#######on receive market data
             ####get most recent market data for ticker
-            
-            ticker1MarketData:list[pd.DataFrame] = self.tickers2Snapshots[ticker1]
-            ticker2MarketData:list[pd.DataFrame] = self.tickers2Snapshots[ticker2]
+
+            ticker1MarketData:list[pd.DataFrame] = self.tickers2Snapshots['stocks'][ticker1]
+            ticker2MarketData:list[pd.DataFrame] = self.tickers2Snapshots['futures_quotes'][ticker2]
+            ticker2MarketData_trades:list[pd.DataFrame] = self.tickers2Snapshots['futures_trades'][ticker2]
+
             ticker1RecentMarketData = None
             ticker2RecentMarketData = None
-
+            ticker2RecentMarketData_trades = None
 
             if len(ticker1MarketData) > 0:
                 
@@ -124,6 +121,8 @@ class SampleDummyStrategy(QuantStrategy):
             })
             
 
+            if len(ticker2MarketData_trades) > 0:
+                ticker2RecentMarketData_trades = ticker2MarketData_trades[-1]
 
             if ticker1RecentMarketData is not None and ticker2RecentMarketData is not None:
                 #########do some calculation with the recent market data
@@ -135,7 +134,7 @@ class SampleDummyStrategy(QuantStrategy):
                     date=now.date(),
                     submissionTime=now.time()
                 )
-                sampleOrder1.orderID = f"{self.getStratID()}-2610-{str(uuid1())}"
+                sampleOrder1.orderID = f"{self.getStratID()}-{ticker1}-{str(uuid1())}"
                 sampleOrder1.type = "MO"
                 sampleOrder1.currStatus = "New"
                 sampleOrder1.currStatusTime = now.time()
@@ -148,7 +147,7 @@ class SampleDummyStrategy(QuantStrategy):
                     date=now.date(),
                     submissionTime=now.time()
                 )
-                sampleOrder2.orderID = f"{self.getStratID()}-3374-{str(uuid1())}"
+                sampleOrder2.orderID = f"{self.getStratID()}-{ticker2}-{str(uuid1())}"
                 sampleOrder2.type = "LO"
                 sampleOrder2.currStatus = "New"
                 sampleOrder2.currStatusTime = now.time()
@@ -157,8 +156,7 @@ class SampleDummyStrategy(QuantStrategy):
                 sampleOrder2.price = ticker2RecentMarketData['bidPrice5'].item()
                 sampleOrder2.stratID = self.getStratID()
 
-                
-                
+
                 ######return a list
                 return [sampleOrder1,sampleOrder2]
         else:
@@ -181,16 +179,19 @@ class SampleDummyStrategy(QuantStrategy):
 
 
             ######do something
+            if order.currStatus == "PartiallyFilled":
+                from datetime import datetime
+                now = datetime.now()
+                sampleOrder3 = order.copyOrder()
+                sampleOrder3.type = "CANCEL"
+                return [sampleOrder3]
 
-            ####e.g. issue cancel order
-            # if order.type == "LO":
-            #     order.type = "CANCEL"
             return []
+
+
 
         return []
 
-
-                
 
 class InDevelopingStrategy(QuantStrategy):
     def __init__(self, stratID, stratName, stratAuthor, ticker, day,
