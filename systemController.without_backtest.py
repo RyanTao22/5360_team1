f5fc38a1bcad6c0fbc09d11828ae3e54348227e4
Wesley Lau <wesleyklhk@yahoo.com.hk>
2024-07-28 11:56:35 +0800

    added UnifiedDataService

--- systemController.without_backtest.py
+++ systemController.without_backtest.py
@@ -6,6 +6,8 @@ Created on Thu Jun 20 10:26:05 2020
 """
 
 from multiprocessing import Process, Queue,Value
+
+from UnifiedDataService import UnifiedDataService
 from marketDataService import MarketDataService
 from futureDataService import FutureDataService
 from exchangeSimulator import ExchangeSimulator
@@ -51,6 +53,24 @@ if __name__ == '__main__':
 
 
     ##########v2
+    # startDate, endDate, startTime, stockCodes, futuresCodes, playSpeed, initial_cash, debug, backTest = ('2024-06-28',
+    #                                                                                                          '2024-06-28',
+    #                                                                                                          132315869,
+    #                                                                                                          ['2610'],
+    #                                                                                                          ['NEF1'],
+    #                                                                                                          10000000,
+    #                                                                                                          1000000,
+    #                                                                                                          False,
+    #                                                                                                          True)
+
+    # Process(name='md', target=MarketDataService, args=(marketData_2_exchSim_q, marketData_2_platform_q, startDate, endDate, startTime, stockCodes, playSpeed, backTest, isReady)).start()
+    # Process(name='futured', target=FutureDataService, args=(futureData_2_exchSim_q, marketData_2_platform_q, startDate, endDate, startTime, futuresCodes, playSpeed, backTest, isReady)).start()
+    # Process(name='stockExchange', target=ExchangeSimulator, args=(marketData_2_exchSim_q, platform_2_exchSim_order_q, exchSim_2_platform_execution_q, stockCodes, isReady, debug)).start()
+    # Process(name='futureExchange', target=ExchangeSimulator, args=(futureData_2_exchSim_q, platform_2_futuresExchSim_order_q, exchSim_2_platform_execution_q, futuresCodes, isReady, debug)).start()
+    #
+    # Process(name='platform', target=TradingPlatform, args=(marketData_2_platform_q, platform_2_exchSim_order_q, platform_2_futuresExchSim_order_q, exchSim_2_platform_execution_q, stockCodes, futuresCodes, initial_cash, analysis_q, isReady, debug)).start()
+
+    #########v3
     startDate, endDate, startTime, stockCodes, futuresCodes, playSpeed, initial_cash, debug, backTest = ('2024-06-28',
                                                                                                              '2024-06-28',
                                                                                                              132315869,
@@ -60,18 +80,19 @@ if __name__ == '__main__':
                                                                                                              1000000,
                                                                                                              False,
                                                                                                              True)
-
-    Process(name='md', target=MarketDataService, args=(marketData_2_exchSim_q, marketData_2_platform_q, startDate, endDate, startTime, stockCodes, playSpeed, backTest, isReady)).start()
-    Process(name='futured', target=FutureDataService, args=(futureData_2_exchSim_q, marketData_2_platform_q, startDate, endDate, startTime, futuresCodes, playSpeed, backTest, isReady)).start()
+    fds = FutureDataService(futureData_2_exchSim_q, marketData_2_platform_q, startDate, endDate, startTime, futuresCodes, playSpeed, backTest, isReady)
+    mds = MarketDataService(marketData_2_exchSim_q, marketData_2_platform_q, startDate, endDate, startTime, stockCodes, playSpeed, backTest, isReady)
+    Process(name='uds', target=UnifiedDataService, args=(mds,fds)).start()
+    # Process(name='futured', target=FutureDataService, args=(futureData_2_exchSim_q, marketData_2_platform_q, startDate, endDate, startTime, futuresCodes, playSpeed, backTest, isReady)).start()
     Process(name='stockExchange', target=ExchangeSimulator, args=(marketData_2_exchSim_q, platform_2_exchSim_order_q, exchSim_2_platform_execution_q, stockCodes, isReady, debug)).start()
     Process(name='futureExchange', target=ExchangeSimulator, args=(futureData_2_exchSim_q, platform_2_futuresExchSim_order_q, exchSim_2_platform_execution_q, futuresCodes, isReady, debug)).start()
 
     Process(name='platform', target=TradingPlatform, args=(marketData_2_platform_q, platform_2_exchSim_order_q, platform_2_futuresExchSim_order_q, exchSim_2_platform_execution_q, stockCodes, futuresCodes, initial_cash, analysis_q, isReady, debug)).start()
 
     #############playground
-    # FutureDataService(futureData_2_exchSim_q, futureData_2_platform_q)
-    # MarketDataService(marketData_2_exchSim_q, marketData_2_platform_q)
-
+    # fds = FutureDataService(futureData_2_exchSim_q, marketData_2_platform_q, startDate, endDate, startTime, futuresCodes, playSpeed, backTest, isReady)
+    # mds = MarketDataService(marketData_2_exchSim_q, marketData_2_platform_q, startDate, endDate, startTime, stockCodes, playSpeed, backTest, isReady)
+    # uds = UnifiedDataService(mds=mds,fds=fds)
 
     # isReady.value = int(input())
     # Process(name='sim', target=ExchangeSimulator, args=(marketData_2_exchSim_q, platform_2_exchSim_order_q, exchSim_2_platform_execution_q, futureData_2_exchSim_q)).start()
